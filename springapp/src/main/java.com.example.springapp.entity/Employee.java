e303b3aa71f11902316a70c9123f0a2f721afd2b
Iamneo <techadmin@iamneo.ai>
2023-09-21 05:58:27 +0000

    Backup Commit

--- springapp/src/main/java.com.example.springapp.entity/Employee.java
+++ springapp/src/main/java.com.example.springapp.entity/Employee.java
@@ -1,5 +1,9 @@
+package com.example.springapp.entity;
 
 
 public class  Employee {
-    
+   private int employeeId ;
+   private String employeeName;
+   private String employeeMail;
+   private long employeeMobile;
 }
