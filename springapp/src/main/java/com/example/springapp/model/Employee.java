0726c74bde3bbecdf365558004b1587b31837100
Iamneo <techadmin@iamneo.ai>
2023-09-28 23:43:32 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/model/Employee.java
+++ springapp/src/main/java/com/example/springapp/model/Employee.java
@@ -1,4 +1,4 @@
-
+package com.example.springapp.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- springapp/src/main/java/com/example/springapp/repository/EmployeeRepo.java
+++ springapp/src/main/java/com/example/springapp/repository/EmployeeRepo.java
@@ -1,4 +1,4 @@
-
+package com.example.springapp.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
